6f1ef5bbf7a848d0fcf765814c7d92fea42ea142
jam <41418470+Sardonyx001@users.noreply.github.com>
2025-02-06 15:10:51 +0900

    refact: use date-fns for date string formatting (default to yy-MM-dd)

--- src/utils/date-format.ts
+++ src/utils/date-format.ts
@@ -1,35 +1,28 @@
-export const getCurrentDateString = () => {
-    // Get the current date
-    const currentDate = new Date();
+import { format } from "date-fns";
 
-    // Get the day, month, and year from the current date
-    const day = String(currentDate.getDate()).padStart(2, '0');
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so we add 1
-    const year = currentDate.getFullYear();
-
-    // Format the date string in "DD-MM-YYYY" format
-    const formattedDate = `${day}-${month}-${year}`;
-
-    return formattedDate;
-}
+export const getCurrentDateString = (formatStr: string = "yyyy-MM-dd") => {
+  // Get the current date
+  const currentDate = new Date();
+  return format(currentDate, formatStr);
+};
 
 export const getCurrentDateTimeString = () => {
-    // Get the current date
-    const currentDate = new Date();
+  // Get the current date
+  const currentDate = new Date();
 
-    // Get the hours and minutes from the current date
-    const hours = String(currentDate.getHours()).padStart(2, '0');
-    const minutes = String(currentDate.getMinutes()).padStart(2, '0'); // Months are zero-based, so we add 1
+  // Get the hours and minutes from the current date
+  const hours = String(currentDate.getHours()).padStart(2, "0");
+  const minutes = String(currentDate.getMinutes()).padStart(2, "0"); // Months are zero-based, so we add 1
 
-    // Format the date string in "DD-MM-YYYY" format
-    const formattedDate = `${getCurrentDateString()} ${hours}:${minutes}`;
+  // Format the date string in "DD-MM-YYYY" format
+  const formattedDate = `${getCurrentDateString()} ${hours}:${minutes}`;
 
-    return formattedDate;
-}
+  return formattedDate;
+};
 
 export const formatDayMonth = (date: Date) => {
-    const day = String(date.getDate());
-    const month = String(date.getMonth()+1);
+  const day = String(date.getDate());
+  const month = String(date.getMonth() + 1);
 
-    return day.padStart(2, '0') + '-' + month.padStart(2, '0');
-}
+  return day.padStart(2, "0") + "-" + month.padStart(2, "0");
+};
